90af0b8ed84b3b4b64bcb0834a9c10a63a2a3d3f
Sunny996 <satlaakash@gmail.com>
2022-04-18 17:29:49 +0530

    REFACTOR: To add a test case for happy message in constructor & no parametes in method

--- src/test/java/com/moodanalyser/MoodAnalyseTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyseTest.java
@@ -1,14 +1,21 @@
 package com.moodanalyser;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MoodAnalyseTest {
     @Test
-    public void WhenGivenASadMessageInConstructorShouldReturnSad(){
+    public void WhenGivenASadMessageInConstructorShouldReturnSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
-        String mood=moodAnalyser.analyseMood();
-        assertEquals(mood,"SAD");
+        String mood = moodAnalyser.analyseMood();
+        assertEquals(mood, "SAD");
+    }
+    @Test
+    public void WhenGivenASadMessageInConstructorShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
+        String mood = moodAnalyser.analyseMood();
+        assertEquals(mood, "HAPPY");
     }
 
 
